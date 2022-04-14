71ea8ec5f0e573d75fdd74f8cc71cb2957dde5fc
Ramis Yüksel <ramisyk97@gmail.com>
2022-04-15 00:03:32 +0300

    create multiple layout structure

--- ECommerce-UI/src/app/app-routing.module.ts
+++ ECommerce-UI/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: 'admin', component: LayoutComponent, children: [
+    {path: '', component: DashboardComponent},
+    {path: 'customers', loadChildren : () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule)},
+    {path: 'products', loadChildren : () => import("./admin/components/products/products.module").then(module => module.ProductsModule)},
+    {path: 'orders', loadChildren : () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule)},
+
+  ] 
+},
+  {path: '', component: HomeComponent},
+  {path: 'cart', loadChildren : () => import('./ui/components/carts/carts.module')},
+  {path: 'cart', loadChildren : () => import('./ui/components/products/products.module')},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

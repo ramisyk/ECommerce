8999001b1e2e440cbfc88fedaf45e1aaf166418d
Ramis Yüksel <ramisyk97@gmail.com>
2022-04-14 23:31:33 +0300

    rotates of components are crated in their own module file

--- ECommerce-UI/src/app/admin/components/dashboard/dashboard.module.ts
+++ ECommerce-UI/src/app/admin/components/dashboard/dashboard.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -9,7 +9,10 @@ import { DashboardComponent } from './dashboard.component';
     DashboardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', component: DashboardComponent}
+    ])
   ]
 })
 export class DashboardModule { }

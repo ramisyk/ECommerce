72c43fa5d2cef7ef5d14a773bc153f7efe6440c2
Ramis Yüksel <ramisyk97@gmail.com>
2022-04-16 16:03:40 +0300

    miltiple layout streuctue is created

--- ECommerce-UI/src/app/app-routing.module.ts
+++ ECommerce-UI/src/app/app-routing.module.ts
@@ -6,17 +6,17 @@ import { HomeComponent } from './ui/components/home/home.component';
 
 const routes: Routes = [
 
-  {path: 'admin', component: LayoutComponent, children: [
-    {path: '', component: DashboardComponent},
-    {path: 'customers', loadChildren : () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule)},
-    {path: 'products', loadChildren : () => import("./admin/components/products/products.module").then(module => module.ProductsModule)},
-    {path: 'orders', loadChildren : () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule)},
-
-  ] 
-},
-  {path: '', component: HomeComponent},
-  {path: 'cart', loadChildren : () => import('./ui/components/carts/carts.module')},
-  {path: 'cart', loadChildren : () => import('./ui/components/products/products.module')},
+  {
+    path: 'admin', component: LayoutComponent, children: [
+      { path: '', component: DashboardComponent },
+      { path: 'customers', loadChildren: () => import("./admin/components/customers/customers.module").then(module => module.CustomersModule) },
+      { path: 'products', loadChildren: () => import("./admin/components/products/products.module").then(module => module.ProductsModule) },
+      { path: 'orders', loadChildren: () => import("./admin/components/orders/orders.module").then(module => module.OrdersModule) },
+    ]
+  },
+  { path: '', component: HomeComponent },
+  { path: 'carts', loadChildren: () => import('./ui/components/carts/carts.module').then(module => module.CartsModule) },
+  { path: 'products', loadChildren: () => import('./ui/components/products/products.module').then(module => module.ProductsModule) },
 ];
 
 @NgModule({

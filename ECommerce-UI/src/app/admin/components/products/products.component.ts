c898137fcd13b086c77cb4f5f3a8e47b5ab6d099
Ramis Yüksel <ramisyk97@gmail.com>
2022-04-25 23:52:23 +0300

    product contract is created for receiving api

--- ECommerce-UI/src/app/admin/components/products/products.component.ts
+++ ECommerce-UI/src/app/admin/components/products/products.component.ts
@@ -17,13 +17,14 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     // this.showSpinner(SpinnerType.BallAtom);
 
-    this.httpClientService.get({
-      baseUrl: "https://jsonplaceholder.typicode.com",
-      controller: "posts"
-    }).subscribe(data => console.log(data))
     // this.httpClientService.get({
-    //   controller: "products"
+    //   baseUrl: "https://jsonplaceholder.typicode.com",
+    //   controller: "posts"
     // }).subscribe(data => console.log(data))
+
+    this.httpClientService.get({
+      controller: "products"
+    }).subscribe(data => console.log(data))
   }
 
 }

4419adb17a5f1f39c64f11427c4f80d0d007df78
Ramis Yuksel <ramisyk97@gmail.com>
2022-05-21 23:18:49 +0300

    rename of uploaded file is fixed

--- ECommerce-API/Infrastructure/ECommerceAPI.Infrastructure/Services/FileService.cs
+++ ECommerce-API/Infrastructure/ECommerceAPI.Infrastructure/Services/FileService.cs
@@ -20,7 +20,7 @@ namespace ECommerceAPI.Infrastructure.Services
             try
             {
                 await using FileStream fileStream = new(path, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, useAsync: false);
-                await fileStream.CopyToAsync(fileStream);
+                await file.CopyToAsync(fileStream);
                 await fileStream.FlushAsync();
 
                 return true;
@@ -47,6 +47,40 @@ namespace ECommerceAPI.Infrastructure.Services
                 else
                 {
                     newFileName = fileName;
+                    int indexNo1 = newFileName.IndexOf('-');
+                    if (indexNo1 == -1)
+                    {
+                        newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extention}";
+                    }
+                    else
+                    {
+                        int lastIndex = 0;
+                        while (true)
+                        {
+                            lastIndex = indexNo1;
+                            indexNo1 = newFileName.IndexOf("-", indexNo1 +1);
+                            if (indexNo1 == -1)
+                            {
+                                indexNo1 = lastIndex;
+                                break;
+                            }
+                        }
+                        int indexNo2 = newFileName.IndexOf('.');
+                        string fileNo = newFileName.Substring(indexNo1+1, indexNo2 - indexNo1-1);
+                        if (int.TryParse(fileNo, out int _fileNo))
+                        {
+                            _fileNo++;
+                            newFileName = newFileName.Remove(indexNo1+1, indexNo2 - indexNo1 - 1)
+                                                 .Insert(indexNo1+1, _fileNo.ToString());
+                        }
+                        else
+                        {
+                            newFileName = $"{Path.GetFileNameWithoutExtension(newFileName)}-2{extention}";
+                        }
+
+                        // mvc-.txt
+                        // 012345
+                    }
                 }
 
                 if (File.Exists($"{path}\\{newFileName}"))
@@ -74,7 +108,7 @@ namespace ECommerceAPI.Infrastructure.Services
 
             foreach (IFormFile file in files)
             {
-                string fileNewName = await FileRenameAsync(file.FileName);
+                string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
                 bool result = await FileCopyAsync($"{uploadPath}\\{fileNewName}", file);
 
                 datas.Add((fileNewName, $"{uploadPath}\\{fileNewName}"));
